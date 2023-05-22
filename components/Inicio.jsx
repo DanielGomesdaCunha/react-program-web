30e4462d71f3939b9c66d55ade6767434e069150
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-05-22 08:19:10 -0300

    Adicionando o componente Inicio e estilizando

---
+++ components/Inicio.jsx
@@ -0,0 +1,11 @@
+export default function Inicio() {
+    return (
+        <section id="inicio">
+            <div className="row justify-content-start">
+                <h1>Sejam bem-vindos</h1>
+                <br />
+                <h2>Sua maior experiência de sua vida</h2>
+            </div>
+        </section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Inicio from '../../components/Inicio'
 
 export default function Home() {
   return (
@@ -11,6 +12,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        <Inicio />
+      </div>
       {/* <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
