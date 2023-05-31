95457cce940e2f15d874f3debe6912d09a962361
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-05-31 08:15:16 -0300

    Instalando e configurando o Bootstrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import "@/styles/style.css";
+import { useEffect } from "react";
 import Head from "next/head";
-import Script from "next/script";
 import Layout from "../../components/Layout";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, [])
+
   return (
     <>
       <Head>
@@ -13,8 +18,6 @@ export default function App({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
-
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossOrigin="anonymous"></Script>
     </>
   );
 }
